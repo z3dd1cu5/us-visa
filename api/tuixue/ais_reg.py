fcc189905d3a476d097ff3925243b885e5b7daae
z3dd1cu5 <z3dd1cu5@protonmail.com>
2021-05-03 22:37:43 +0800

    Update AIS code

--- api/tuixue/ais_reg.py
+++ api/tuixue/ais_reg.py
@@ -3,126 +3,38 @@ import time
 import json
 import random
 import requests
+import traceback
+from bs4 import BeautifulSoup
 from threading import Lock
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
 from . import global_var as g
 from . import config
 
-wait_timeout = 20
-refresh_interval = 30
-
-chrome_options = Options()
-chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-chrome_options.add_experimental_option('useAutomationExtension', False)
-chrome_options.add_argument("--headless")
-
+wait_timeout = 120
 cache = {}
 
 def register(country_code, email, password, node):
     global cache
     lock = g.value(email + "_lock", Lock())
     with lock:
-        # Login
         try:
-            c_service = Service("/usr/bin/chromedriver")
-            c_service.command_line_args()
-            c_service.start()
-            if len(node) > 0:
-                entry = node
-            else:
-                selenium_list = [x.strip() for x in open("node.txt", "r").readlines()]
-                entry = random.choice(selenium_list)
-            driver = webdriver.Remote(
-                command_executor='http://%s:4444/wd/hub' % entry,
-                desired_capabilities=chrome_options.to_capabilities()
-            )
-            driver.set_window_size(1036 + random.randint(0, 20), 583 + random.randint(0, 20))
-            print("Choose Node:", entry)
-            driver.get("https://ais.usvisa-info.com")
-
+            new_session = None
             if email in cache:
                 session, schedule_id, group_id = cache[email]
                 new_session = change_region(country_code, session, group_id)
-                driver.add_cookie({'name' : '_yatri_session', 'value' : new_session, 'path' : '/', 'domain': 'ais.usvisa-info.com', 'secure': True})
-                driver.get("https://ais.usvisa-info.com/%s/niv/groups/%s" % (country_code, group_id))
-            else:
-                ais_ng_lock = g.value("ais_ng_lock", Lock())
-                with ais_ng_lock:
-                    try:
-                        r = requests.get(config.get("ais_ng_api") + "?code=%s&email=%s&pswd=%s" % (country_code, email, password), timeout=180)
-                    except:
-                        pass
-                new_session = ""
-                cookies_list = json.loads(r.text)
-                if not type(cookies_list) == list:
-                    return cookies_list["code"], None, None
-                for item in cookies_list:
-                    if item.get("name") == "_yatri_session":
-                        new_session = item.get("value")
 
-                driver.add_cookie({'name' : '_yatri_session', 'value' : new_session, 'path' : '/', 'domain': 'ais.usvisa-info.com', 'secure': True})
-                driver.get("https://ais.usvisa-info.com/%s/niv" % country_code)
+            if not new_session:
+                code, new_session = ais_ng(country_code, email, password)
+                if not new_session:
+                    return code, None, None
 
-            def wait_loading(xpath, option="locate"):
-                try:
-                    if option == "locate":
-                        element_present = EC.presence_of_element_located((By.XPATH, xpath))
-                    elif option == "clickable":
-                        element_present = EC.element_to_be_clickable((By.XPATH, xpath))
-                    WebDriverWait(driver, wait_timeout).until(element_present)
-                except TimeoutException:
-                    print("Timed out waiting for page to load")
-                    # driver.execute_script("window.scrollTo(0, 1080)")
-                    # driver.save_screenshot("test.png")
+            group_id, schedule_id, new_session = account_page(new_session)
+            if not schedule_id:
+                return None, None, None
+            content, session = payment_page(new_session, schedule_id)
+            if not content:
+                return None, None, None
 
-            # Continue
-            continue_button_xpath = "//a[contains(text(), 'Continue')]"
-            wait_loading(continue_button_xpath)
-            current_url = driver.current_url
-            group_id = current_url.split("/")[-1]
-            continue_button = driver.find_element_by_xpath(continue_button_xpath)
-            continue_button.click()
-
-            # Choose action 
-            pay_button_xpath = "//a[contains(text(), 'Pay Visa Fee')]"
-            wait_loading(pay_button_xpath)
-            banner = driver.find_element_by_tag_name('h5')
-            banner.click()
-            wait_loading(pay_button_xpath, option="clickable")
-            pay_button = driver.find_element_by_xpath(pay_button_xpath)
-            pay_button.click()
-
-            # Collect result
-            title_xpath = "//h2[contains(text(), 'MRV Fee Details')]"
-            wait_loading(title_xpath)
-            time_table = driver.find_element_by_class_name('for-layout')
-            result = []
-            if time_table:
-                trs = time_table.find_elements_by_tag_name('tr')
-                for tr in trs:
-                    tds = tr.find_elements_by_tag_name('td')
-                    if not len(tds) == 2:
-                        continue
-                    place = tds[0].text
-                    date_str = tds[1].text
-                    s = date_str.split()
-                    year, month, day = 0, 0, 0
-                    if len(s) >= 3 and s[0] != "No":
-                        day_str, month_str, year_str = s[-3], s[-2].replace(",", ""), s[-1]
-                        year, month, day = int(year_str), g.MONTH[month_str], int(day_str)
-                    result.append([place, (year, month, day)])
-
-            current_url = driver.current_url
-            schedule_id = current_url.split("/")[-2]
-            session = driver.get_cookie("_yatri_session")["value"]
+            result = parse_date(content)
             if result:
                 cache[email] = [session, schedule_id, group_id]
             else:
@@ -131,13 +43,8 @@ def register(country_code, email, password, node):
         except Exception as e:
             if email in cache:
                 del cache[email]
-            print(str(e))
+            traceback.print_exc()
             return None, None, None
-        finally:
-            if driver:
-                driver.quit()
-            if c_service:
-                c_service.stop()
 
 
 def change_region(country_code, session, group_id):
@@ -150,5 +57,99 @@ def change_region(country_code, session, group_id):
     r = req.get("https://ais.usvisa-info.com/%s/niv/groups/%s" % (country_code, group_id), headers=headers)
     if r.status_code != 200:
         print("Error")
+    if "Sign Out" not in r.text:
+        return None
     session = r.cookies["_yatri_session"]
     return session
+
+
+def ais_ng(country_code, email, password):
+    ais_ng_lock = g.value("ais_ng_lock", Lock())
+    with ais_ng_lock:
+        try:
+            r = requests.get(config.get("ais_ng_api") + "?code=%s&email=%s&pswd=%s" % (country_code, email, password), timeout=wait_timeout)
+        except:
+            pass
+    new_session = ""
+    cookies_list = json.loads(r.text)
+    if not type(cookies_list) == list:
+        return cookies_list["code"], None
+    for item in cookies_list:
+        if item.get("name") == "_yatri_session":
+            new_session = item.get("value")
+            return 0, new_session
+    return None, None
+
+
+def account_page(new_session):
+    r = requests.get(
+        "https://ais.usvisa-info.com/en-gb/niv/account", 
+        cookies={
+            "_yatri_session": new_session
+        }, 
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+            "Referer": "https://ais.usvisa-info.com/en-gb/niv/users/sign_in"
+        },
+        allow_redirects=False
+    )
+    if r.status_code != 302:
+        return None, None, None
+    group_id = r.headers["Location"].split("/")[-1]
+    new_session = r.cookies["_yatri_session"]
+
+    r = requests.get(
+        "https://ais.usvisa-info.com/en-gb/niv/groups/" + group_id, 
+        cookies={
+            "_yatri_session": new_session
+        }, 
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+            "Referer": "https://ais.usvisa-info.com/en-gb/niv/users/sign_in"
+        }
+    )
+    if r.status_code != 200:
+        return None, None, None
+    soup = BeautifulSoup(r.text, "html.parser")
+    link = soup.find('a', text="Continue").get('href')
+    schedule_id = link.split("/")[-2]
+    new_session = r.cookies["_yatri_session"]
+    return group_id, schedule_id, new_session
+
+
+def payment_page(new_session, schedule_id):
+    r = requests.get(
+        "https://ais.usvisa-info.com/en-gb/niv/schedule/%s/payment" % schedule_id, 
+        cookies={
+            "_yatri_session": new_session
+        }, 
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+            "Referer": "https://ais.usvisa-info.com/en-gb/niv/schedule/%s/continue_actions" % schedule_id
+        }
+    )
+    if r.status_code != 200:
+        return None, None
+    session = r.cookies["_yatri_session"]
+    return r.text, session
+
+
+def parse_date(content):
+    soup = BeautifulSoup(content, "html.parser")
+    time_table = soup.find("table", {"class": "for-layout"})
+    result = []
+    if time_table:
+        trs = time_table.find_all('tr')
+        for tr in trs:
+            tds = tr.find_all('td')
+            if not len(tds) == 2:
+                continue
+            place = tds[0].text
+            date_str = tds[1].text
+            s = date_str.split()
+            year, month, day = 0, 0, 0
+            if len(s) >= 3 and s[0] != "No":
+                day_str, month_str, year_str = s[-3], s[-2].replace(",", ""), s[-1]
+                year, month, day = int(year_str), g.MONTH[month_str], int(day_str)
+            result.append([place, (year, month, day)])
+    return result
